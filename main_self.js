48c962650fe94db6125d715dadaca5efc0dc3770
Eunju <wealthinkang.gmail.com>
2024-02-19 23:16:29 +0900

    first commit

--- main_self.js
+++ main_self.js
@@ -1,19 +1,9 @@
-/**
-*  브라우저에서 지원하는 기능을 Web Api라 한다.
-*  (Ajax, fetch, setTimeout, eventhandler)
-* 
-*  ★★ async() - async를 통해서 비동기 함수를 만들 수 있다.
-*  ★★ await - 시간 걸리는 코드를 기다려줄 수 있다.
-*/
 
-//뉴스를 가져오는 함수
-
-const API_KEY = `ea02c6c81d7a411692d739a2f9e1dac1`;
 let news = [];
 
 const getLatestNews = async () => {
 	const url = new URL(
-		`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines`
+		`https://jju-news-api.netlify.app/top-headlines`
 	);
 	const response = await fetch(url); //url data를 가져오는 호출 함수 fetch()
 	const data = await response.json(); //json은 객체를 텍스트화 시킨 파일 형태 확장자
@@ -23,7 +13,7 @@ const getLatestNews = async () => {
 const getNewsByKeyword = async () => {
 	// URL 객체를 생성할 때는 new 키워드를 반드시 사용해야 합니다.
 	const url = new URL(
-		`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?q=아이유`
+		`https://jju-news-api.netlify.app/top-headlines?q=아이유`
 	);
 	const response = await fetch(url);
 	const data = await response.json();
@@ -32,7 +22,7 @@ const getNewsByKeyword = async () => {
 
 const getNewsByCategory = async () => {
 	const url = new URL(
-		`http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?category=science`
+		`https://jju-news-api.netlify.app/top-headlines?category=science`
 	);
 	const response = await fetch(url);
 	const data = response.json();
