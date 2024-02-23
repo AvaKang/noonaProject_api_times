0f954cddd2aecc037083e0a94355c2c04ef2ccb7
Eunju <wealthinkang.gmail.com>
2024-02-24 01:52:30 +0900

    first commit

--- main_self.js
+++ main_self.js
@@ -10,17 +10,33 @@ menus.forEach(function(menu) {
 }); 
 */
 
+//뉴스 url 전역 변수 (코드 리펙토링)
+let url = new URL(`https://jju-news-api.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
+
 // 뉴스 가져오기
-const getNews = async (url) => {
-	const response = await fetch(url); //url data를 가져오는 호출 함수 fetch()
-	const data = await response.json(); //json은 객체를 텍스트화 시킨 파일 형태 확장자
-	newsList = data.articles;
-	render();
-}
+const getNews = async () => {
+	try{
+		//에러 검사하고 싶은 모든 코드 넣어주자!
+		const response = await fetch(url); //url data를 가져오는 호출 함수 fetch()
+		
+		const data = await response.json(); //json은 객체를 텍스트화 시킨 파일 형태 확장자
+		if(response.status === 200) {
+			if(data.articles.length === 0){
+				throw new Error('No result for this search');
+			}
+			newsList = data.articles;
+			render();
+		} else {
+			throw new Error(data.message)
+		}
+	}catch(error){
+		errorRender(error.message);
+	}
+};
 
 // api 가져오기
 const getLatestNews = () => {
-	const url = new URL(`https://jju-news-api.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
+	url = new URL(`https://jju-news-api.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
 											//`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
 	getNews(url);
 };
@@ -28,7 +44,7 @@ const getLatestNews = () => {
 // 카테고리 기사 가져오기
 const getNewsByCategory = (event) => {
 	const category = event.target.textContent.toLowerCase();
-	const url = new URL(`https://jju-news-api.netlify.app/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`); //카테고리 추가
+	url = new URL(`https://jju-news-api.netlify.app/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`); //카테고리 추가
 											//http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?category=science
 	getNews(url);
 }
@@ -36,7 +52,7 @@ const getNewsByCategory = (event) => {
 // 키워드 기사 가져오기
 const getNewsByKeyword = () => {
 	const keyword = document.getElementById('mobile-input-data').value;
-	const url = new URL(`https://jju-news-api.netlify.app/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`); //키워드 추가
+	url = new URL(`https://jju-news-api.netlify.app/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`); //키워드 추가
 											//http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?q=아이유
 	getNews(url);
 }
@@ -62,8 +78,18 @@ const render = () => {
 	).join('');
 
 	document.getElementById('news-board').innerHTML = newsHtml;
-
 };
+
+//에러 메시지 출력
+const errorRender = (errorMessage) => {
+	const errorHTML = `<div class="alert alert-danger" role="alert">
+  ${errorMessage}
+</div>`;
+
+document.getElementById('news-board').innerHTML = errorHTML
+};
+
+
 
 /* mobile menus */
 let $mobileMenuButton = document.getElementById('mobileMenuButton');
@@ -102,3 +128,50 @@ $mobileSearchButton.addEventListener('click', (event) => {
 			
 getLatestNews();
 render();
+
+/* =================에러 핸들링================= */
+/**
+ * 1. try문 안에 있는 코드를 실행 하다가 에러가 나면
+ * 2. catch 안에 들어가 에러를 실행
+ * 3. 에러가 없다면 catch블락을 무시하고 넘어간다.
+ * Error메시지 : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
+ */
+let weight = 29;
+try{
+	//코드를 입력
+	//이 안에서 에러가 발생하면
+	if(weight < 30) {
+		throw new Error('당신은너무 말랐어'); //에러가 발생되는 순간 try 코드는 끝이남
+	}
+}catch(error){
+	//try안에 발생하는 에러를 catch가 잡아주는 곳
+	//catch는 error을 매개변수로 받는다.
+	//console.log('내가 잡은 에러는', error.message)
+}
+
+/* ================리펙토링 사용================= */
+let message = '';
+
+const martHtml = () => {
+	message = `<div class="row message">누나 가전에 와주셔서 감사하고 항상 건강하세요!</div>`;
+
+	document.getElementById('message-wrap').innerHTML = message;
+}
+
+const buyTv = () => {
+	console.log('TV 구매 감사드립니다.')
+	martHtml();
+}
+
+const buyAC = () => {
+	console.log('AC 구매 감사드립니다.')
+	martHtml();
+}
+
+const buyCellphone = () => {
+	console.log('phone 구매 감사드립니다.')
+	martHtml();
+}
+
+martHtml();
+
