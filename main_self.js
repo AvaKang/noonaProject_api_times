225304608787964a31975e002d1c264b35be812e
Eunju <wealthinkang.gmail.com>
2024-02-24 23:28:04 +0900

    first commit

--- main_self.js
+++ main_self.js
@@ -4,21 +4,33 @@ const menus = document.querySelectorAll('.menus button');
 menus.forEach(menu => menu.addEventListener('click',(event) => getNewsByCategory(event))) //ES6 버전
 
 //뉴스 url 전역 변수 (코드 리펙토링)
-let url = new URL(`https://jju-api-times-error.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
+let url = new URL(`https://jju-api-times-pagination.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
+
+//페이지 네이션 totalResults 변수
+let totalResults = 0; //기본값 0으로 설정
+let page = 1; //현제 보고있는 페이지 1
+const pageSize = 10; //한번에 10개의 데이터 가져오기
+const groupSize = 5; //페이지네이션을 5개씩 보여주기
 
 // 뉴스 가져오기
 const getNews = async () => {
 	try{
+		//url 호출 전에 세팅하고  fetch를 호출해줘야 한다.
+		url.searchParams.set('page', page); // 1.ulr뒤에 2.&page=page(페이지 파라미터를 세팅)을 붙인 후 3.fetch 실행
+		url.searchParams.set('pageSize', pageSize); // 1.url뒤에 2.&pageSize=pageSize(페이지 파라미터를 세팅) 을 붙인 후 3.fetch 실행
+
 		//에러 검사하고 싶은 모든 코드 넣어주자!
 		const response = await fetch(url); //url data를 가져오는 호출 함수 fetch()
-		
 		const data = await response.json(); //json은 객체를 텍스트화 시킨 파일 형태 확장자
 		if(response.status === 200) {
 			if(data.articles.length === 0){
 				throw new Error('No result for this search');
 			}
 			newsList = data.articles;
+			totalResults = data.totalResults; //totalResults
 			render();
+			paginationRender();
+			console.log(data);
 		} else {
 			throw new Error(data.message)
 		}
@@ -29,24 +41,24 @@ const getNews = async () => {
 
 // api 가져오기
 const getLatestNews = () => {
-	url = new URL(`https://jju-api-times-error.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
-											//`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+	url = new URL(`https://jju-api-times-pagination.netlify.app/top-headlines?country=kr&apiKey=${API_KEY}`);
+							//`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
 	getNews(url);
 };
 
 // 카테고리 기사 가져오기
 const getNewsByCategory = (event) => {
 	const category = event.target.textContent.toLowerCase();
-	url = new URL(`https://jju-api-times-error.netlify.app/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`); //카테고리 추가
-											//http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?category=science
+	url = new URL(`https://jju-api-times-pagination.netlify.app/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`); //카테고리 추가
+							//http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?category=science
 	getNews(url);
 }
 
 // 키워드 기사 가져오기
 const getNewsByKeyword = () => {
 	const keyword = document.getElementById('mobile-input-data').value;
-	url = new URL(`https://jju-api-times-error.netlify.app/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`); //키워드 추가
-											//http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?q=아이유
+	url = new URL(`https://jju-api-times-pagination.netlify.app/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`); //키워드 추가
+								//http://times-node-env.eba-appvq3ef.ap-northeast-2.elasticbeanstalk.com/top-headlines?q=아이유
 	getNews(url);
 }
 
@@ -60,31 +72,29 @@ const imgError = (image) => {
 const render = () => {
 	const newsHtml = newsList.map(news => `<div class="row news-list">
 		<div class="col-lg-4 news-image">
-			<img src="${news.urlToImage}" alt="뉴스 이미지" class="news-img-size" onerror="imgError(this)">
+		<img src="${news.urlToImage}" alt="뉴스 이미지" class="news-img-size" onerror="imgError(this)">
 		</div>
 		<div class="col-lg-8 news-content">
-			<h2 class="news-title">${news.title}</h2>
-			<p>${news.description == null || news.description == "" ? "내용없음": news.description.length > 200 ? news.description.substring(0, 200) + "..." : news.description}</p>
-			<div>${news.source.name || "no source"}  ${moment(news.publishedAt).fromNow()}</div>
+		<h2 class="news-title">${news.title}</h2>
+		<p>${news.description == null || news.description == "" ? "내용없음": news.description.length > 200 ? news.description.substring(0, 200) + "..." : news.description}</p>
+		<div>${news.source.name || "no source"}  ${moment(news.publishedAt).fromNow()}</div>
 		</div>
-	</div>`
+		</div>`
 	).join('');
-
+	
 	document.getElementById('news-board').innerHTML = newsHtml;
 };
 
 //에러 메시지 출력
 const errorRender = (errorMessage) => {
 	const errorHTML = `<div class="alert alert-danger" role="alert">
-  ${errorMessage}
-</div>`;
-
-document.getElementById('news-board').innerHTML = errorHTML
+	${errorMessage}
+	</div>`;
+	
+	document.getElementById('news-board').innerHTML = errorHTML
 };
 
-
-
-/* mobile menus */
+/* ==============mobile slide menus============== */
 let $mobileMenuButton = document.getElementById('mobileMenuButton');
 let $mobileMenus = document.getElementById('mobileMenus');
 let $mobileMenuClose = document.getElementById('mobileMenuClose');
@@ -109,26 +119,26 @@ $mobileSearchButton.addEventListener('click', (event) => {
 	} else {
 		$mobileSearchForm.classList.add('on');		
 	} */
-
+	
 	/** toggle 방법3
-	 * 검색아이콘을 안 눌렀을 경우에는 queryInput.disabled = true;
-	 * 검색아이콘을 눌렀을 경우에는 queryInput.disabled = false;
-	 */
+	* 검색아이콘을 안 눌렀을 경우에는 queryInput.disabled = true;
+	* 검색아이콘을 눌렀을 경우에는 queryInput.disabled = false;
+	*/
 });
 
 //콤마 없애는 방법 (구글검색: array to string javascript)
 //join은 map함수와 자주 같이 쓰인다.
-			
+
 getLatestNews();
 render();
 
 /* =================에러 핸들링================= */
 /**
- * 1. try문 안에 있는 코드를 실행 하다가 에러가 나면
- * 2. catch 안에 들어가 에러를 실행
- * 3. 에러가 없다면 catch블락을 무시하고 넘어간다.
- * Error메시지 : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
- */
+* 1. try문 안에 있는 코드를 실행 하다가 에러가 나면
+* 2. catch 안에 들어가 에러를 실행
+* 3. 에러가 없다면 catch블락을 무시하고 넘어간다.
+* Error메시지 : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Error
+*/
 let weight = 29;
 try{
 	//코드를 입력
@@ -147,7 +157,7 @@ let message = '';
 
 const martHtml = () => {
 	message = `<div class="row message">누나 가전에 와주셔서 감사하고 항상 건강하세요!</div>`;
-
+	
 	document.getElementById('message-wrap').innerHTML = message;
 }
 
@@ -168,3 +178,81 @@ const buyCellphone = () => {
 
 martHtml();
 
+/* ==============페이지 네이션================= */
+/**
+ * 페이지 네이션 작업에 알아야 할 공식
+ * 
+ * //api에서 알 수 있는 정보
+ * 1. totalResults : 총 몇개의 결과가 있는지
+ * 2. page : 현재 보고있는 페이지
+ * 
+ * //우리가 정해야 하는 정보
+ * 1. pageSize : 한번에 몇개의 데이터를 가져올지(api문서에서 정한 기본값은 20개이다)
+ * 2. groupSize : 한번에 몇개의 페이지를 페이지네이션에서 보여줄지
+ * 
+ * //우리가 계산해야 하는 정보
+ * 1. totalPage : 전체 페이지가 몇개인지 구하는 방법 (Math.ceil(totalResults / pageSize))
+ * 									소수점 올리는 함수 Math.ceil()
+ * 2. pageGroup : 몇번째 페이지 그룹에 속해 있는지 구하는 방법 (Math.ceil(pageGroup / groupSize))
+ * 3. lastPage : 마지막값 구하는 방법 ((pageGroup * groupSize))
+ * 4. firstPage : 첫번째 페이지 구하는 방법 (마지막 - (groupSize - 1))
+ */
+
+	/**
+	 * URL에 쿼리 더하기
+	 * URL 객체로 선언한 url(new URL())을 써서 선언한 url은 URL객체에서 제공하는 다양한 함수들을 쓸 수 있다.
+	 * .URLSearchParams()는 query에 있는 파라미터들을 추가, 삭제 하거나 값을 가져오는 등 다양한 작업이 가능하다.
+	 * 참고url: https://developer.mozilla.org/en-US/docs/Web/API/URLSearchParams
+	 */
+
+const paginationRender = () => {
+	//totalResults
+	//page
+	//pageSize
+	//groupSize
+	//totalPages
+	const totalPages = Math.ceil(totalResults/pageSize);
+
+	//pageGroup
+	const pageGroup = Math.ceil(page/groupSize);
+	console.log('pageGroup', pageGroup)
+	
+	//lastPage
+	let lastPage = pageGroup * groupSize
+	//만약 마지막 페이지 그룹이 그룹사이즈 보다 작다면? lastPage = totalPages으로 변경
+	if(lastPage > totalPages) {
+		lastPage = totalPages;
+	}
+
+	//firstPage
+	const firstPage = lastPage - (groupSize - 1) <= 0 ? 1 : lastPage - (groupSize - 1);
+	console.log('firstPage', firstPage)
+
+	let paginationHTML = `<li class="page-item" onclick="moveToPage(${page-1})"><a class="page-link" href="#">Previous</a></li>`;
+
+	for(let i = firstPage; i < lastPage; i++){
+		paginationHTML += `<li class="page-item ${
+			i === page ? 'active' : ''
+		}" onclick="moveToPage(${i})"><a class="page-link">${i}</a></li>`
+	}
+
+	paginationHTML += `<li class="page-item" onclick="moveToPage(${page+1})"><a class="page-link" href="#">Next</a></li>`;
+	document.querySelector('.pagination').innerHTML = paginationHTML
+
+	// <nav aria-label="Page navigation example">
+	// 	<ul class="pagination">
+	// 		<li class="page-item"><a class="page-link" href="#">Previous</a></li>
+	// 		<li class="page-item"><a class="page-link" href="#">1</a></li>
+	// 		<li class="page-item"><a class="page-link" href="#">2</a></li>
+	// 		<li class="page-item"><a class="page-link" href="#">3</a></li>
+	// 		<li class="page-item"><a class="page-link" href="#">Next</a></li>
+	// 	</ul>
+	// </nav>
+};
+
+const moveToPage = (pageNum) => {
+	console.log('moveToPage', pageNum);
+	page = pageNum;
+	getNews()
+};
+paginationRender();
